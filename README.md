7821fd498b09bc94ed458b4a0b6666eb9dcd9433
Jarek Gawor <jgawor@gmail.com>
2019-04-18 22:49:09 -0400

    more updates

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
    ```
    sudo pip3 install google-auth google-auth-httplib2 google-api-python-client
    ```
+1. Setup crontab
+   ```
+   crontab crontab.sample
+   ```
--- capture_video.sh
+++ capture_video.sh
@@ -16,3 +16,5 @@ ls -1 *.jpg | sort | awk '{ print "file", $1 }' > image.list
 ffmpeg -r 5 -f concat -i "image.list" -vcodec libx264 $VIDEO_DIR/$TIMESTAMP.mp4
 
 cat image.list  | sed 's/file//' | xargs rm -v
+
+$CUR_DIR/google_drive_upload.py $VIDEO_DIR/$TIMESTAMP.mp4
